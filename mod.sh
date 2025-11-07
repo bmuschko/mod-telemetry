ca14b23453e22887fa3ebb02e67e30b3db6acbf6
Benjamin Muschko <benjamin.muschko@gmail.com>
2025-11-07 07:32:28 -0700

    Use full path

--- mod.sh
+++ mod.sh
@@ -8,7 +8,7 @@ set -e
 # Configuration - Use environment variables if set, otherwise use defaults
 MOD_JAR="${MOD_JAR:-mod.jar}"  # Path to the Moderne CLI JAR
 MIN_VERSION="3.45.0"  # Set minimum required version (versions less than 3.45.0 do not generate telemetry metrics)
-TELEMETRY_DIR="$(pwd)/.moderne"  # Telemetry directory relative to current working directory
+TELEMETRY_DIR="$(pwd)/.moderne/cli/trace"  # Telemetry directory relative to current working directory
 BI_ENDPOINT="${BI_ENDPOINT:-}"  # Set your BI system endpoint URL
 
 # Authentication configuration (optional)
